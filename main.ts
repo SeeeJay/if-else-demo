2368e935e2315f338c516532e623f0f7a1983a63
SeeeJay <113055585+SeeeJay@users.noreply.github.com>
2022-09-19 14:29:13 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    number = randint(0, 2)
+    if (number == 0) {
+        basic.showIcon(IconNames.Sad)
+    } else if (number == 1) {
+        basic.clearScreen()
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    number = randint(0, 2)
+    if (number == 0) {
+        basic.showIcon(IconNames.Sad)
+        music.playMelody("C5 A B G A F G E ", 120)
+    } else if (number == 1) {
+        basic.showIcon(IconNames.Happy)
+        music.playMelody("E D G F B A C5 B ", 120)
+        basic.clearScreen()
+    } else if (number == 2) {
+        basic.showIcon(IconNames.Happy)
+        music.playMelody("E D G F B A C5 B ", 120)
+        basic.clearScreen()
+    }
+})
+let number = 0
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
